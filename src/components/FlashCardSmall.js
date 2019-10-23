452110ac53b913ba648ad8640a4b2e0d5784f97e
Justin Pfeifer <japfeifer@fortlewis.edu>
2019-10-23 07:53:50 -0600

    added grid css

--- src/components/FlashCardSmall.js
+++ src/components/FlashCardSmall.js
@@ -2,27 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class FlashCardSmall extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDevice = this.handleDevice.bind(this);
-    }
-
-    handleDevice() {
-        if(this.props.device === 'Desktop') {
-            return 'col-1-of-4'
-        } else if(this.props.device === 'Tablet Landscape') {
-            return 'col-1-of-3'
-        } else if (this.props.device === 'Tablet Portrait') {
-            return 'col-1-of-2'
-        } else {
-            return '';
-        }
-    }
-
     render() {       
         return (
             <Link
-                className={this.handleDevice() + ` flashcard-small flashcard-small-link`}
+                className='flashcard-small flashcard-small-link'
                 to={`/numbers/${this.props.number}`}>{this.props.number}
             </Link>
         )
