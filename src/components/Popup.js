f45d57775fc7f1f3b2a22188592a605c600e163a
Justin Pfeifer <japfeifer@fortlewis.edu>
2019-11-01 15:51:10 -0600

    syncing with master

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -16,11 +16,45 @@ class Popup extends React.Component {
         })
     }
 
+    emojiGrade() {
+        const total = this.props.numCorrect + this.props.numWrong;
+        const percent = (this.props.numCorrect / total) * 100;
+
+        if(percent >= 90) {
+            return (
+                <div>
+                    <p>Good Job!</p>
+                    <svg className='Popup-icon'>
+                        <use xlinkHref='/img/sprite.svg#icon-emoji-happy'></use>
+                    </svg>
+                    <p>Keep up the good work!</p>
+                </div>
+            )
+        } else if(percent < 90 && percent >=70) {
+            return (
+                <div>
+                <p>Ooops, you answered {this.props.numCorrect} out of {total} correctly.</p>
+                <svg className='Popup-icon'>
+                    <use xlinkHref='/img/sprite.svg#icon-emoji-neutral'></use>
+                </svg>
+                <p>Good work and keep on practicing!</p>
+            </div>
+            )
+        } else return (
+            <div>
+                <p>Ooops, you answered {this.props.numCorrect} out of {total} correctly</p>
+                <svg className='Popup-icon'>
+                    <use xlinkHref='/img/sprite.svg#icon-emoji-sad'></use>
+                </svg>
+                <p>Try again and keep on practicing!</p>
+            </div>
+        )
+    }
+
     render() {
         const total = this.props.numCorrect + this.props.numWrong;
         const percent = (this.props.numCorrect / total) * 100;
         const percent_formatted = percent.toFixed(2);
-
         if(!this.props.showPopup || this.state.hide) {
             return null;
         }
@@ -34,15 +68,8 @@ class Popup extends React.Component {
                                 <use xlinkHref='/img/sprite.svg#icon-stopwatch'></use>
                             </svg>
                             </h2>
-                        <h3 className='heading-tertiary'>Your grade is:</h3>
-                        <p>You answered {this.props.numCorrect} correctly:</p>
-                        {this.props.numWrong <= 0 
-                        ? 
-                            <p>Good Job, you didn't miss any problems!</p>
-                         : 
-                            <p>Ooops, you missed {this.props.numWrong} out of {total}</p>
-                        }
-                        <p>You received {percent_formatted}%</p>
+                        <h3 className='heading-tertiary'>You scored {percent_formatted}%</h3>
+                        {this.emojiGrade()}
                     </div>
                     <div className='Popup-buttons'>
                         <Link to={`/numbers/${this.props.name}`} onClick={() => window.location.reload(false)} className='btn btn-secondary mr-sm'>Retry</Link>
